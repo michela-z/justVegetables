6ae4a2ddbc56dbd0c20635447b5d9b6ec91bc76a
Michela Zallocco <michelazallocco94@gmail.com>
2022-09-07 18:09:23 +0200

    update

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import FavoritesContext from '../FavoritesContext';
 import { Link } from 'react-router-dom';
