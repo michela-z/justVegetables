5f6e4fb06c4352564b2a658ba03d31585a2d4316
Michela Zallocco <michelazallocco94@gmail.com>
2022-09-07 21:21:04 +0200

    update

--- src/FavoritesContext.js
+++ src/FavoritesContext.js
@@ -6,10 +6,10 @@ export function FavoritesProvider({ children }) {
 
     const [ favorite, setFavorite ] = useState([]);
 
-    useEffect(() => {
-        const recipesFavorites = JSON.parse(localStorage.getItem('favorites-recipes'));
-        setFavorite(recipesFavorites);
-    }, []);
+    // useEffect(() => {
+    //     const recipesFavorites = JSON.parse(localStorage.getItem('favorites-recipes'));
+    //     setFavorite(recipesFavorites);
+    // }, []);
 
     const saveToLocalStorage = (items) => {
         localStorage.setItem('favorites-recipes', JSON.stringify(items))
