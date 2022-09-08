253c1ad2d540b5f88d1a944064ec402cbf018b97
Michela Zallocco <michelazallocco94@gmail.com>
2022-09-08 12:57:05 +0200

    update

--- src/FavoritesContext.js
+++ src/FavoritesContext.js
@@ -12,7 +12,7 @@ export function FavoritesProvider({ children }) {
 
     useEffect(() => {
         const recipesFavorites = JSON.parse(localStorage.getItem('favorites-recipes'));
-        if (favorite) {
+        if (recipesFavorites) {
             console.log('esiste')
             setFavorite(recipesFavorites);
         } else {
